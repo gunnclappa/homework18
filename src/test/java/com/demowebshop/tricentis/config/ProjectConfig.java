1c46be4fe5b04ec961e00869e235c12c5ca13743
gunnclappa <kayratwutangov@gmail.com>
2022-10-10 16:03:25 +0500

    Change email and chrome version

--- src/test/java/com/demowebshop/tricentis/config/ProjectConfig.java
+++ src/test/java/com/demowebshop/tricentis/config/ProjectConfig.java
@@ -13,7 +13,7 @@ public interface ProjectConfig extends Config {
     @DefaultValue("chrome")
     String browser();
 
-    @DefaultValue("91.0")
+    @DefaultValue("100")
     String browserVersion();
 
     @DefaultValue("1920x1080")
